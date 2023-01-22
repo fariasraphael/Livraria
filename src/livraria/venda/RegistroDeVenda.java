9c667b0d04bc30e93fa45b77e5cec905e3030a39
fariasraphael <farias.raphael@gmail.com>
2023-01-22 20:43:08 -0300

    crio classe Registro de venda e metodo de venda

---
+++ src/livraria/venda/RegistroDeVenda.java
@@ -0,0 +1,27 @@
+package livraria.venda;
+
+import livraria.estoque.Estoque;
+import livraria.estoque.produtos.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistroDeVenda {
+
+    private Double dinheiroNocaixa = 0.0;
+
+    public Double getDinheiroNocaixa() {
+        return dinheiroNocaixa;
+    }
+
+    public <P extends Produto> void venderProdutos(CarrinhoDeCompras carrinho, Estoque estoque) {
+
+        for (int i = 0; i < carrinho.getCarrinho().size(); i++) {
+            if (estoque.verificadorDeItemEmEstoque(carrinho.getCarrinho().get(i))) {
+                this.dinheiroNocaixa += carrinho.getCarrinho().get(i).getPreco();
+                estoque.reduzirQuantidadeDoItemNoEstoque(carrinho.getCarrinho().get(i));
+            }
+        }
+        carrinho.getCarrinho().clear();
+    }
+}
