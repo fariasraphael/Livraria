1632a432eceac0172ee8064027e8e711b696f7a6
fariasraphael <farias.raphael@gmail.com>
2023-01-22 20:43:28 -0300

    refatoro array do carrinho de compras

--- src/livraria/venda/CarrinhoDeCompras.java
+++ src/livraria/venda/CarrinhoDeCompras.java
@@ -6,22 +6,34 @@ import livraria.estoque.produtos.Produto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarrinhoDeCompras <P extends Produto> {
+public class CarrinhoDeCompras {
     private Double dinheiro;
-    List<P> carrinho = new ArrayList<>();
+    private List<Produto> carrinho = new ArrayList<>();
 
-    private Boolean adicionarProdutoAoCarrinho (Estoque e, P produto){
+    public List<Produto> getCarrinho() {
+        return carrinho;
+    }
+
+    public <P extends Produto> void adicionarProdutoAoCarrinho (Estoque e, P produto){
        if (e.verificadorDeItemEmEstoque(produto)) {
            carrinho.add(produto);
        } else {
            System.out.println("Produto não disponível em estoque");
        }
     }
+    public <P extends Produto> void retrirarProdutoDoCarrinho (P produto) {
+        if (carrinho.contains(produto)){
+            carrinho.remove(produto);
+        } else {
+            System.out.println("Produto não está no carrinho");
+        }
+    }
 
-    private void verItensDoCarrinho (){
+    public void verItensDoCarrinho (){
         if(carrinho.isEmpty()) {
             System.out.println("Seu carrinho está vazio");
         } else {
+            System.out.println("Itens no carrinho:");
             carrinho.forEach(System.out::println);
         }
     }
